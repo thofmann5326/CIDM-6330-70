a82bed9d8e67856e0416a9bc61fff168e585056e
thofmann5326 <Trevorhofmann5326@gmail.com>
2024-01-30 13:42:44 -0600

    Main

---
+++ CIDM6330-SPRING2024/projects/APP2023/code-chapter_06_uow/src/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="allocation",
+    version="0.6",
+    packages=["allocation"],
+)
---
+++ CIDM6330-SPRING2024/projects/acme/domain/weather/forecast.py
@@ -0,0 +1,5 @@
+class forecast:
+    def __init__(self) -> None:
+        self.raw_text: str = None
+        self.station = None
+        pass
